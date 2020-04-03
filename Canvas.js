9b662eff5c0795f1f3ac5c83e738791d678f6f26
Laszlo Velinszky <laszlo.velinszky@meltwater.com>
2020-04-03 00:06:41 -0700

    Added classes

---
+++ Canvas.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Sprite from './Sprite';
+
+export default function Canvas() {
+    return     <View><Sprite/></View>
+}
---
+++ Sprite.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Sprite() {
+    return     <Text>Change this to image</Text>
+}
