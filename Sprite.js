569725012693819ccc6827a36b2958320cc12711
Laszlo Velinszky <laszlo.velinszky@meltwater.com>
2020-04-07 00:13:28 -0700

    added style

--- Sprite.js
+++ Sprite.js
@@ -2,5 +2,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Sprite() {
-    return     <Text>Change this to image</Text>
+    const styles = StyleSheet.create({
+        container: {
+        borderRadius: 10,
+        borderWidth: 5,
+        backgroundColor: 'lightgrey'
+    },
+        text: {
+            color: 'red',
+            fontSize: 20,
+            margin: 5,
+            textAlign: 'center'
+        }});
+    return     <View style={styles.container}>
+                    <Text style={styles.text}>Box</Text>
+                </View>
 }
